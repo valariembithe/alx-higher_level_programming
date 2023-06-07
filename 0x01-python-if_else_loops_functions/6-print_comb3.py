d0d5eacbd68f62bfe4977c8213b943162a4ebdc4
valariembithe <valmbithe691@gmail.com>
2023-06-07 01:17:07 -0700

    fixed pycodestyle validation

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # prints all possible different combinations of two digits.
-for digit in range(0,10):
+for digit in range(0, 10):
     for digit1 in range(digit + 1, 10):
         if digit == 8 and digit1 == 9:
             print("{}{}".format(digit, digit1))
