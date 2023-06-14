f45d7a72180892be310f3101d7b435f43f5d39c1
valariembithe <valmbithe691@gmail.com>
2023-06-14 05:55:34 -0700

    task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    list_key = list(a_dictionary.keys())
+    dict_copy = a_dictionary.copy()
+    list_key = list(dict_copy.keys())
     list_key.sort()
     for i in list_key:
-        print("{} : {}".format(i, a_dictionary.get(i)))
-
+        print("{} : {}".format(i, dict_copy.get(i)))
