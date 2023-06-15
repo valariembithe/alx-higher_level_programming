5c5b5d46d93ec96351011217c9acb1fc4cd1001e
valariembithe <valmbithe691@gmail.com>
2023-06-15 02:43:44 -0700

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new = a_dictionary.copy()
-    list_key = new.keys()
+    list_key = list(new.keys())
 
     for i in list_key:
         new[i] *= 2
