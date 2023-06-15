0703b3334973be597ff87c3037248b4d5fc0a267
valariembithe <valmbithe691@gmail.com>
2023-06-15 01:21:41 -0700

    task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = key
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
     return (a_dictionary)
