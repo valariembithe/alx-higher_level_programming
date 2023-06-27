364724ea20ca16ada19c32abecf1b818bed0f4c8
valariembithe <valmbithe691@gmail.com>
2023-06-27 03:14:16 -0700

    tasks

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+""" Define class named Square """
 class Square:
+    """ Reprezents Square """
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Define class Square """
+class Square:
+    """ Represent a square """
+    def __init__(self, size=0):
+        """ set size as private attribute """
+        self.__size = size
