0da3cf321361b1d7e7ca0048299ff07bc4476888
valariembithe <valmbithe691@gmail.com>
2023-06-28 03:23:30 -0700

    task 103

---
+++ 0x05-python-exceptions/python.h
@@ -0,0 +1,10 @@
+#ifndef PYTHON_H
+#define PYTHON_H
+#include <stdio.h>
+#include <stdeff.h>
+#include <stdlib.h>
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+#endif
