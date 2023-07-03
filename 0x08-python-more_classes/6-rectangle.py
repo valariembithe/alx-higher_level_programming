6ecb81f1976770aff22bc29e6cd71e817172a505
valariembithe <valmbithe691@gmail.com>
2023-07-03 02:49:26 -0700

    task 6

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""
+    a class defined Rectangle
+    string __str__ representation
+"""
+
+
+class Rectangle:
+    """ a class that defines Rectangle
+        Args: width of type int
+              height of type int
+    """
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """ instances for width and height """
+        self.width = width
+        self.height = height
+        type(self).number_of_instances += 1
+
+    def __del__(self):
+        """ deletes instance of class """
+        print("Bye rectangle...")
+        type(self).number_of_instances -= 1
+
+    @property
+    def width(self):
+        """ set the width to retrive it """
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """ properties of width """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ set the height to retrive it """
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """ properties of height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ return area of rectangle """
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """ return perimeter of a rectangle """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return ((2 * self.__width) + (2 * self.__height))
+
+    def __str__(self):
+        """ returns a rectangle with # """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        pic = "\n".join(["#" * self.__width for rows in range(self.__height)])
+        return pic
+
+    def __repr__(self):
+        """ string representation to create new instance """
+        return ("Rectangle({:d}, {:d})".format(self.width, self.height))
