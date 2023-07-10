3ae629122d4920f330ac84f503aa442f4e247495
valariembithe <valmbithe691@gmail.com>
2023-07-10 03:48:45 -0700

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module for instance """
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Checks if the object is an instance of a class that inherited from
+    """
+    return(isinstance(obj, a_class))
