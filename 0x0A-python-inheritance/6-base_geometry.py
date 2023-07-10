0320cdba3421220792b7a707397baa470a774de8
valariembithe <valmbithe691@gmail.com>
2023-07-10 04:06:22 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module Geometry """
+
+
+class BaseGeometry:
+    """ an empty class """
+    pass
+
+    def area(self):
+        """ gets area """
+        raise Exception("area() is not implemented")
