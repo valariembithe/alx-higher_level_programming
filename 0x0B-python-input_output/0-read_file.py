8d81efac4f28f6ea59e812fce33f3c2c8b7c15a0
valariembithe <valmbithe691@gmail.com>
2023-07-11 03:10:21 -0700

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" read file module """
+
+
+def read_file(filename=""):
+    """ function that reads a text file (UTF8) and prints it to stdout """
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
