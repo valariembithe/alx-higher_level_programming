d734492c0ac4b67ba79d55c40536eab83f67e893
valariembithe <valmbithe691@gmail.com>
2023-07-11 04:15:08 -0700

    task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" json module """
+import json
+
+
+def class_to_json(obj):
+    """returns the dictionary description with simple data structure"""
+    r = {}
+    for key in obj.__dict__:
+        value = getattr(obj, key)
+        if type(value) in [list, dict, int, bool, str]:
+            r[key] = value
+
+    return r
