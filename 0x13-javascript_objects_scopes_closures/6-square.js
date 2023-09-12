7b38f5436aebba23ee4660ff0ab9baa808cb0f49
valariembithe <valmbithe691@gmail.com>
2023-09-12 04:18:31 -0700

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+/* a class named Sqaure */
+const Square1 = require('./5-square');
+
+class Square extends Square1 {
+	charPrint(c) {
+		if (c === undefined) {
+			this.print();
+		} else {
+			for (let i = 0; i < this.height; i++) {
+				console.log('C'.repeat(this.width));
+			}
+		}
+	}
+}
+module.exports = Square;
