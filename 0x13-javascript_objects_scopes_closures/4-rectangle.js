be37d6fb2f0448bc5b0b375bcebfdffcc7a23759
valariembithe <valmbithe691@gmail.com>
2023-09-12 03:40:46 -0700

    Task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+/* A class named Rectangle */
+
+class Rectangle {
+        constructor (w, h) {
+                if (w <= 0 || h <= 0|| isNaN(w) || isNaN(h)) {
+                        return this;
+                }
+                this.width = w;
+                this.height = h;
+        }
+        print() {
+                for (let i = 0; i < this.height; i++) {
+                        console.log('X'.repeat(this.width));
+                }
+        }
+	rotate() {
+		[this.width,this.height] = [this.height, this.width];
+	}
+	double() {
+		this.width = this.width * 2;
+		this.height = this.height *2;
+	}
+}
+module.exports = Rectangle;
