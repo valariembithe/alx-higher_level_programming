d57c1b65c04c1dfb8ce564ffc7f5e9ba8e63cade
valariembithe <valmbithe691@gmail.com>
2023-09-15 05:13:18 -0700

    Task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    script that lists all states from the database hbtn_0e_0_usa
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    """
+        Results must be sorted in ascending order by states.id
+    """
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                        passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states\
+                WHERE name LIKE BINARY 'N%' \
+                ORDER BY states.id ASC")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
