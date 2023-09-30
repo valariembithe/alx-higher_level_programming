8493f333a453618390682dba9f23ee20acc47a9c
valariembithe <valmbithe691@gmail.com>
2023-09-30 03:31:38 -0700

    Taks

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+""" sends a request to the URL and 
+    displays the value of the X-Request-Id variable
+"""
+
+
 import sys
 import urllib.request
-""" sends a request to the URL and displays the value of the X-Request-Id variable"""
-
 
 if __name__ == "__main__":
     url = sys.argv[1]
