1a05b23c26464eb41a36ede51df578a5572abbaf
valariembithe <valmbithe691@gmail.com>
2023-09-30 03:56:39 -0700

    Task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -12,6 +12,6 @@ from requests.auth import HTTPBasicAuth
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    auth = HTTPBAsicAuth(username, password)
+    auth = HTTPBasicAuth(username, password)
     r = requests.get("https://api.github.com/user", auth=auth)
     print(r.json().get("id"))
